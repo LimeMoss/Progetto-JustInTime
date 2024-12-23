97aa45a0d14caf0e1cc47a294466e7d245f1ed20
vince <vincenzogenovese04@gmail.com>
2024-12-23 16:22:13 +0100

    trying to add Tomcat and so JSP

---
+++ ProgettoIs JustInTime/src/main/java/com/justInTime/SpringBootJspApplication.java
@@ -0,0 +1,22 @@
+package com.justInTime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication(scanBasePackages = "static.WEB-INF")
+@ComponentScan("com.justInTime.controller")
+@ComponentScan("com.justInTime.service")
+public class SpringBootJspApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringBootJspApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJspApplication.class);
+    }
+}
