c3418585ad71e3cf3846a02d47a83efd10bc2ca7
your-id <y.dashchuk@studenti.unisa.it>
2025-01-03 16:33:44 +0100

    fixing merge

---
+++ ProgettoIs JustInTime/target/classes/static/script/classificaLocale.js
@@ -0,0 +1,28 @@
+async function loadClassificaLocale() {
+    try {
+        const response = await fetch('/locale');
+        const players = await response.json();
+
+        console.log("Risposta JSON ricevuta:", players);
+
+        const highscoreList = document.getElementById('highscore-list');
+        highscoreList.innerHTML = '';
+
+        players.forEach((player, index) => {
+            const playerDiv = document.createElement('div');
+            playerDiv.classList.add('highscore-players');
+
+            playerDiv.innerHTML = `
+                <p class="position">${index + 1}</p>
+                <p class="name">${player.nome}</p>
+                <p class="points">${player.maxScore} pt</p> 
+            `;
+
+            highscoreList.appendChild(playerDiv);
+        });
+    } catch (error) {
+        console.error('Errore durante il caricamento della classifica:', error);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', loadClassificaLocale);
