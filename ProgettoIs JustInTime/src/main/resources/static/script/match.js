6d74c2917581f9f9a9f5b1b76db5ba5b61469ef1
vince <vincenzogenovese04@gmail.com>
2025-01-08 19:08:31 +0100

    time fixed, doesnt work turns

--- ProgettoIs JustInTime/src/main/resources/static/script/match.js
+++ ProgettoIs JustInTime/src/main/resources/static/script/match.js
@@ -51,7 +51,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     function fetchCurrentPlayer() {
-        fetch('/nameIndexPlayer', {
+        fetch('/game/nameIndexPlayer', {
             method: 'GET',
             credentials: 'include',
         })
